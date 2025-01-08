04c38515dbcc586c56579576f481e63031396bd7
nevengh <56957050+nevengh@users.noreply.github.com>
2025-01-08 01:55:41 -1100

    add her and him section

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import './App.css'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Hero from './components/Hero/Hero';
 import { LanguageProvider } from './LanguageContext';
+import WhatsAppBtn from './components/WhatsAppBtn/WhatsAppBtn';
+import Home from './pages/Home/Home';
 const App = () => {
   return (
     <div>
      <LanguageProvider>
 
-      <Hero/>
+      <Home/>
+      <WhatsAppBtn/>
      </LanguageProvider>
     </div>
   )
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,41 @@
-import './Home.css'
+import { Button } from 'react-bootstrap';
+import Hero from '../../components/Hero/Hero';
+import './Home.css';
+import her from '../../assets/her-img.jpg';
+import him from '../../assets/him-img.jpg';
+import { useState } from 'react';
 
 const Home = () => {
+  const [selectedImage, setSelectedImage] = useState(her);  // HER image as default
+
+  const handleShowHer = () => {
+    setSelectedImage(her);
+  };
+
+  const handleShowHim = () => {
+    setSelectedImage(him);
+  };
+
   return (
-    <div>Home</div>
-  )
+    <div className='Home'>
+      <Hero />
+
+      {/* For Him/Her */}
+      <div className="for-him-her">
+        <h4 className="text-center subhead-title">We Provide for You</h4>
+        <h1 className='text-center head-title'>We Treat All Types of Skin</h1>
+        
+        <div className="choice-btn d-flex align-items-center justify-content-center mt-5">
+          <Button className='me-3' onClick={handleShowHim}>For HIM</Button>
+          <Button onClick={handleShowHer}>For HER</Button>
+        </div>
+        
+        <div className="image-box mt-4">
+          <img src={selectedImage} alt="Selected" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
