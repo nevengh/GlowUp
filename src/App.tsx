5005a2d68b20f9a6a757fc92f7b89da46c9d042e
nevengh <56957050+nevengh@users.noreply.github.com>
2025-01-08 00:49:43 -1100

    hero section and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import './App.css'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Hero from './components/Hero/Hero';
+import { LanguageProvider } from './LanguageContext';
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+     <LanguageProvider>
+
+      <Hero/>
+     </LanguageProvider>
+    </div>
   )
 }
 
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,70 @@
+import "./Hero.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import heroVideo from "../../assets/Sirpi+face-output.webm";
+import logo from "../../assets/glowupLogo-removebg-preview.png";
+import { useLanguage } from "../../LanguageContext";
+import en from "../../locales/en";
+import ar from "../../locales/ar";
+import { Dropdown } from "react-bootstrap";
+const Hero = () => {
+  const { language, setLanguage } = useLanguage();
+  const translations = language === "en" ? en : ar;
+
+  const handleLanguageChange = (lang: "en" | "ar") => {
+    setLanguage(lang);
+  };
+  return (
+    <div className="Hero">
+      <video src={heroVideo} autoPlay muted loop></video>
+      <div className="overlay">
+        <div className="hero-content">
+          <h1 className="hero-heading">
+            Glow Up Your Skin <br /> With Us
+          </h1>
+          <a href="#book" className="hero-btn">
+            Book Now
+          </a>
+        </div>
+        <Navbar expand="lg" className="navbar-overlay">
+          <Container>
+            <Navbar.Brand href="#home">
+              <img src={logo} alt="Glow Up Logo" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="m-auto">
+                <Nav.Link href="#home">About</Nav.Link>
+                <Nav.Link href="#link">Services</Nav.Link>
+                <Nav.Link href="#link">Contact</Nav.Link>
+              </Nav>
+              <div className="left-navbar-section d-flex align-items-center justify-content-center">
+                <a className="me-3" href="#">
+                  Book Now
+                </a>
+                <Dropdown>
+                  <Dropdown.Toggle variant="#CCC964" id="dropdown-basic">
+                    {language === "en"
+                      ? translations.English
+                      : translations.Arabic}
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    <Dropdown.Item onClick={() => handleLanguageChange("en")}>
+                      {translations.English}
+                    </Dropdown.Item>
+                    <Dropdown.Item onClick={() => handleLanguageChange("ar")}>
+                      {translations.Arabic}
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </div>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
